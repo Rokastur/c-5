345bacbf0e23c2969a4ad342e07c2f958ccb29d8
rokastur <rokasatminas@gmail.com>
2020-01-07 03:55:47 +0200

    swapped two variables by using tuples;

--- SwapValues/SwapValues/Program.cs
+++ SwapValues/SwapValues/Program.cs
@@ -10,6 +10,16 @@ namespace SwapValues
     {
         static void Main(string[] args)
         {
+
+            int A = 5;
+            int B = 7;
+
+            Console.WriteLine("A is {0}, B is {1}", A, B);
+
+            (A, B) = (B, A);
+
+            Console.WriteLine("now A is {0} and B is {1}", A, B);
+            Console.ReadLine();
         }
     }
 }

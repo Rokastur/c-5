c700572fd07e7641cfefe46068749146e9164414
rokastur <rokasatminas@gmail.com>
2020-01-07 04:42:30 +0200

    Finished coding.

--- AddDigitsOfANumber/AddDigitsOfANumber/Program.cs
+++ AddDigitsOfANumber/AddDigitsOfANumber/Program.cs
@@ -10,6 +10,18 @@ namespace AddDigitsOfANumber
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Enter a 2 digit number");
+            int a = int.Parse(Console.ReadLine());
+
+            int dig1 = a / 10;
+            int dig2 = a % 10;
+
+            int sum = dig1 + dig2;
+
+            Console.WriteLine("You have entered {0}. Its made up of digits {1} and {2}. The sum of these digits is {3}",a,dig1,dig2,sum);
+            Console.ReadLine();
+
         }
     }
 }

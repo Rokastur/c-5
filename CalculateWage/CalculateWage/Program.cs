bfd9c1dab96910514043d189ce821a36c4bf7ec5
rokastur <rokasatminas@gmail.com>
2020-01-07 02:08:26 +0200

    finished code for calculating wage;

---
+++ CalculateWage/CalculateWage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter employees ID... ");
+            int ID = int.Parse(Console.ReadLine());
+            Console.WriteLine("... and the hours worked... ");
+            int h = int.Parse(Console.ReadLine());
+            Console.WriteLine("... and finally the hourly rate ");
+            double r = double.Parse(Console.ReadLine());
+
+            double wage = h * r;
+            Console.WriteLine("The employee [ID: {0}] has earned a wage of: {1} euros", ID, wage);
+            Console.ReadLine();
+
+
+        }
+    }
+}

a819256591f00d3f83a35c5b4e589dba52925860
rokastur <rokasatminas@gmail.com>
2020-01-07 02:57:08 +0200

    finished code for converting seconds to HH:MM:SS format

---
+++ ConvertSecondsToHoursMinutesSeconds/ConvertSecondsToHoursMinutesSeconds/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertSecondsToHoursMinutesSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter number of seconds");
+            int seconds = int.Parse(Console.ReadLine());
+
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            Console.WriteLine("You have entered seconds equivalent to: {0}", time);
+            Console.ReadLine();
+
+        }
+    }
+}
